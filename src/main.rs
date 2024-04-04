1827729f4d90f49ed7bf383c7846e6c77479862e
George Su <gsu@blend.com>
2024-04-03 19:18:34 -0700

    cleanup unused code

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,25 @@ use automerge::{ObjType, AutoCommit, transaction::Transactable};
 fn log_memory_usage() -> () {
     if let Some(usage) = memory_stats() {
         println!("physical memory usage: {}M", usage.physical_mem / 1000000);
-        // println!("virtual memory usage: {}", usage.virtual_mem);
     }
 }
 
 fn test_automerge() -> () {
     let mut doc = AutoCommit::new();
     let contacts = doc.put_object(automerge::ROOT, "contacts", ObjType::List).unwrap();
-    let alice = doc.insert_object(&contacts, 0, ObjType::Map).unwrap();
-    doc.put(&alice, "name", "Alice").unwrap();
-    doc.put(&alice, "email", "alice@example.com").unwrap();
-    let bob = doc.insert_object(&contacts, 1, ObjType::Map).unwrap();
-    doc.put(&bob, "name", "Bob").unwrap();
-    doc.put(&bob, "email", "bob@example.com").unwrap();
-
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for i in 2..100000 {
-        // println!("i: {}", i);
+    for i in 0..100000 {
         let new_contact = doc.insert_object(&contacts, i, ObjType::Map).unwrap();
         doc.put(&new_contact, "name", generate(50, charset)).unwrap();
         doc.put(&new_contact, "email", generate(50, charset)).unwrap();
     }
-
-    // let data: Vec<u8> = doc.save();
-    // println!("doc vec u8: {}", data.len());
-}
-
-fn _generate_random_strings() -> () {
-    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    for i in 0..100000 {
-        let _random_string = generate(1000, charset);
-    }
+    let data: Vec<u8> = doc.save();
+    println!("doc vec u8: {}", data.len());
 }
 
 fn main() {
     println!("starting main");
     log_memory_usage();
-    // generate_random_strings();
     test_automerge();
     log_memory_usage();
     println!("finished main");
