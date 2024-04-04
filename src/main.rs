c73d332571ec0ab5d2c2cb041babb80427323c7a
George Su <gsu@blend.com>
2024-04-03 18:36:41 -0700

    log memory usage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use memory_stats::memory_stats;
 use automerge::{ObjType, AutoCommit, transaction::Transactable};
 
 fn main() {
     println!("starting main");
+
+    if let Some(usage) = memory_stats() {
+        println!("Current physical memory usage: {}", usage.physical_mem);
+        println!("Current virtual memory usage: {}", usage.virtual_mem);
+    }
+
     let mut doc = AutoCommit::new();
     let contacts = doc.put_object(automerge::ROOT, "contacts", ObjType::List).unwrap();
     let alice = doc.insert_object(&contacts, 0, ObjType::Map).unwrap();
@@ -11,5 +18,11 @@ fn main() {
     doc.put(&bob, "name", "Bob").unwrap();
     doc.put(&bob, "email", "bob@example.com").unwrap();
     let _data: Vec<u8> = doc.save();
+
+    if let Some(usage) = memory_stats() {
+        println!("Current physical memory usage: {}", usage.physical_mem);
+        println!("Current virtual memory usage: {}", usage.virtual_mem);
+    }
+
     println!("finished main");
 }
