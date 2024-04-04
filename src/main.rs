38f6c21e2ed075e818ba92695df4ac6bf2d3eaf7
George Su <gsu@blend.com>
2024-04-03 18:32:42 -0700

    use example from https://docs.rs/automerge/latest/automerge/

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use automerge::{ObjType, AutoCommit, transaction::Transactable};
+
 fn main() {
-    println!("Hello, world!");
+    println!("starting main");
+    let mut doc = AutoCommit::new();
+    let contacts = doc.put_object(automerge::ROOT, "contacts", ObjType::List).unwrap();
+    let alice = doc.insert_object(&contacts, 0, ObjType::Map).unwrap();
+    doc.put(&alice, "name", "Alice").unwrap();
+    doc.put(&alice, "email", "alice@example.com").unwrap();
+    let bob = doc.insert_object(&contacts, 1, ObjType::Map).unwrap();
+    doc.put(&bob, "name", "Bob").unwrap();
+    doc.put(&bob, "email", "bob@example.com").unwrap();
+    let _data: Vec<u8> = doc.save();
+    println!("finished main");
 }
